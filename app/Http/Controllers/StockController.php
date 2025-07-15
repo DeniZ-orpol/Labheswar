4212abf9157d7cd9b69d61726b2a7e5625e418da
Nikhil Patel <nikhil@nectarbits.com>
2025-07-15 10:20:43 +0530

    view file

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -3,16 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Models\Stock;
+use App\Models\Inventory;
+use App\Models\Product;
+use App\Models\Purchase;
+use App\Models\PurchaseParty;
+use App\Models\PurchaseReceipt;
+use App\Traits\BranchAuthTrait;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\ValidationException;
 
 class StockController extends Controller
 {
+     use BranchAuthTrait;
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $auth = $this->authenticateAndConfigureBranch();
+        $user = $auth['user'];
+        $branch = $auth['branch'];
+        $role = $auth['role'];
+
+        if (strtoupper($role->role_name) === 'SUPER ADMIN') {
+            $parties = PurchaseParty::get();
+            $purchaseReceipt = PurchaseReceipt::with(['purchaseParty', 'createUser', 'updateUser'])
+                ->orderByDesc('id')->paginate(10);
+        } else {
+            $parties = PurchaseParty::on($branch->connection_name)->get();
+            $purchaseReceipt = PurchaseReceipt::on($branch->connection_name)
+                ->with(['purchaseParty', 'createUser', 'updateUser'])
+                ->orderByDesc('id')->paginate(10);
+        }
+
+        return view('purchase.index', compact(['parties', 'purchaseReceipt']));
     }
 
     /**
